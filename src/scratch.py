aa581e0ce9c3eef7078c55df82efe34a2195fc94
Gregory Bolla <greg.bolla@Gregorys-MacBook-Pro.local>
2018-10-18 08:56:02 -0600

    init commit

---
+++ src/scratch.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+csv = pd.read_csv("/Users/greg.bolla/Desktop/git-projects/kaggle-center-for-policing-equity/data/raw/Dept_11-00091/11-00091_ACS_data/11-00091_ACS_education-attainment/ACS_16_5YR_S1501_metadata.csv")
+
+print(csv.head())
